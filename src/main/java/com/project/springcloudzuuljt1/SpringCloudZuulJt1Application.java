4f2395d207bdfcc5726548f78f756ccfe5326e87
Emerson Millard <emillard.code@gmail.com>
2022-07-23 12:00:00 -0700

    Modified main class with annotations and controller method.

--- src/main/java/com/project/springcloudzuuljt1/SpringCloudZuulJt1Application.java
+++ src/main/java/com/project/springcloudzuuljt1/SpringCloudZuulJt1Application.java
@@ -2,12 +2,24 @@ package com.project.springcloudzuuljt1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringCloudZuulJt1Application {
 
+    @GetMapping("/getDiagnosis")
+    public String getDoctorService() {
+
+        return "Diagnosis microservice called...";
+
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(SpringCloudZuulJt1Application.class, args);
+
     }
 
 }
