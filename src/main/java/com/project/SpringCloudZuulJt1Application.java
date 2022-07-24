3247a226021950df37a2deea3aa5e2bb5f61defb
Emerson Millard <emillard.code@gmail.com>
2022-07-24 12:00:00 -0700

    Moved controller method from main class to controller class.

--- src/main/java/com/project/SpringCloudZuulJt1Application.java
+++ src/main/java/com/project/SpringCloudZuulJt1Application.java
@@ -2,19 +2,9 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringCloudZuulJt1Application {
-
-    @GetMapping("/getDiagnosis")
-    public String getDoctorService() {
-
-        return "Diagnosis microservice called...";
-
-    }
 
     public static void main(String[] args) {
 
