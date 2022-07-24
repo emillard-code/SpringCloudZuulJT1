8c403a7a90beb8b3986ec5726a14ad42b3bf4d97
Emerson Millard <emillard.code@gmail.com>
2022-07-24 12:00:00 -0700

    Moved directory of main class.

--- src/main/java/com/project/SpringCloudZuulJt1Application.java
+++ src/main/java/com/project/SpringCloudZuulJt1Application.java
@@ -1,4 +1,4 @@
-package com.project.springcloudzuuljt1;
+package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
